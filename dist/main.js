b9adb151c88f2aab62969a43f57fa7f401619607
CRONEX-Technologies <cronex.technologies@gmail.com>
2025-11-07 15:30:02 +0100

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,17 @@
-const textEl = document.getElementById("text");
-const text = "Coming soon...";
-let i = 0;
-function type() {
+document.addEventListener("DOMContentLoaded", () => {
+  const textEl = document.getElementById("text");
+  const text = "Coming soon...";
+  if (!textEl) return;
+
+  let i = 0;
+
+  function type() {
     if (i < text.length) {
-        textEl.textContent += text.charAt(i);
-        i++;
-        setTimeout(type, 120);
+      textEl.textContent += text.charAt(i);
+      i++;
+      setTimeout(type, 120);
     }
-}
-document.addEventListener("DOMContentLoaded" () => {
-    setTimeout(type, 1500);
+  }
+
+  setTimeout(type, 1500);
 });
-export {};
-//# sourceMappingURL=main.js.map
